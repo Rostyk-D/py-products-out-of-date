3acfa074907b194edd563fd4594e24d65bda5aa4
Rostyk <rostik.durjagin@gmail.com>
2026-03-30 16:11:14 +0300

    fix flake

--- app/test_main.py
+++ app/test_main.py
@@ -57,8 +57,7 @@ from app.main import outdated_products
         ),
     ]
 )
-def test_outdated_products(mocked_today: datetime,
-                           products: list,
-                           expected: list) -> None:
-    with patch("app.main.datetime.date.today", return_value=mocked_today):
+def test_outdated_products(mocked_today: datetime, products: list[dict], expected:list[str]) -> None:
+    with patch("app.main.datetime.date") as mock_date:
+        mock_date.today.return_value = mocked_today
         assert outdated_products(products) == expected
